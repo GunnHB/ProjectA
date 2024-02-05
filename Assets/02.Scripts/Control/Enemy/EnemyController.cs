286ccad158e99713e610d06e0d297ddc99690eb5
GunnHB <gunn0467@gmail.com>
2024-02-05 12:24:25 +0900

    [02/05] 적 추가

--- Assets/02.Scripts/Control/Enemy/EnemyController.cs
+++ Assets/02.Scripts/Control/Enemy/EnemyController.cs
@@ -1,13 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
 
 using BehaviorTree;
-using System;
+
+using Sirenix.OdinInspector;
 
 public class EnemyController : MonoBehaviour
 {
+    [Title("[WayPoints]")]
+    [SerializeField] private bool _setRandom = true;
+    [SerializeField, HideIf(nameof(_setRandom))]
+    protected List<GameObject> _wayPointList = new();
+    [SerializeField, ShowIf(nameof(_setRandom))]
+    protected float _radius = 5f;
+
     // Components
     private Movement _movement;
     private Animator _animator;
@@ -16,29 +25,33 @@ public class EnemyController : MonoBehaviour
     private float _speed = 3f;
 
     // BahaviorTree
-    private BehaviorTreeRunner _runner;
+    private BehaviorTreeRunner _btRunner;
 
     protected List<INode> _rootNodeList;
 
     protected List<INode> _alertNodeList;
     protected List<INode> _patrolNodeList;
 
+    private bool _missTarget = false;                   // 추격 대상을 놓침
+
+    private Vector3 _currWayPoint = Vector3.zero;       // 현재 이동 중인 지점의 벡터
+    private int _currListIndex;                         // waypoint 리스트 인덱스
+
     private void Awake()
     {
         _movement = GetComponent<Movement>();
         _animator = GetComponent<Animator>();
 
-        _runner = new BehaviorTreeRunner(SettingBT());
+        _btRunner = new BehaviorTreeRunner(SettingBT());
     }
 
     private void Update()
     {
-        _runner.Operate();
+        _btRunner.Operate();
     }
 
     private INode SettingBT()
     {
-        // throw new NotImplementedException();
         return EntireNode();
     }
 
@@ -46,7 +59,7 @@ public class EnemyController : MonoBehaviour
     {
         _rootNodeList = new List<INode>();
 
-        _rootNodeList.Add(AlertNode());
+        // _rootNodeList.Add(AlertNode());
         _rootNodeList.Add(PatrolNode());
 
         return new SelectorNode(_rootNodeList);
@@ -78,7 +91,57 @@ public class EnemyController : MonoBehaviour
 
     private INode.ENodeState DoPatrol()
     {
-        throw new NotImplementedException();
+        if (_missTarget)
+            _missTarget = false;
+
+        if (_setRandom)
+            SetWayPointRandom();
+        else
+            SetWayPointByList();
+
+        if (_currWayPoint == null || _currWayPoint == Vector3.zero)
+            return INode.ENodeState.FailureState;
+
+        var distance = Vector3.SqrMagnitude(_currWayPoint = transform.position);
+
+        if (distance <= .1f)
+        {
+            _currWayPoint = Vector3.zero;
+
+            return INode.ENodeState.SuccessState;
+        }
+
+        _movement.SetDirection(_currWayPoint);
+        _movement.SetSpeed(_speed);
+
+        _movement.MovementUpdate();
+
+        return _currWayPoint != Vector3.zero ? INode.ENodeState.RunningState : INode.ENodeState.FailureState;
+    }
+
+    private Vector3 SetWayPointRandom()
+    {
+        if (_currWayPoint == Vector3.zero)
+        {
+            // 현재 위치에서 랜덤한 지점을 반환
+            var randomSite = (UnityEngine.Random.insideUnitSphere * _radius) + transform.position;
+
+            _currWayPoint = new Vector3(randomSite.x, 0f, randomSite.z);
+        }
+
+        return _currWayPoint;
+    }
+
+    private Vector3 SetWayPointByList()
+    {
+        _currListIndex++;
+
+        if (_wayPointList.Count >= _currListIndex)
+            _currListIndex = 0;
+
+        _currWayPoint = _wayPointList[_currListIndex].transform.localPosition;
+
+        return _currWayPoint;
     }
 
     private INode.ENodeState DoIdle()
