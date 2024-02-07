5ef5f4689fff112df6bfb5d56b4291f706b86fef
GunnHB <gunn0467@gmail.com>
2024-02-07 18:52:24 +0900

    [02/07] Equipment.cs 추가

---
+++ Assets/02.Scripts/Control/Equipment.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : MonoBehaviour
+{
+    [SerializeField] private GameObject _handHolder;
+    [SerializeField] private GameObject _sheathHolder;
+    [SerializeField] private GameObject _weapon;
+
+    private GameObject _currWeaponInHand;
+    private GameObject _currWeaponInSheath;
+
+    private void Start()
+    {
+        _currWeaponInSheath = Instantiate(_weapon, _sheathHolder.transform);
+    }
+
+    public void DrawWeapon()
+    {
+        _currWeaponInHand = Instantiate(_weapon, _handHolder.transform);
+        Destroy(_currWeaponInSheath);
+    }
+
+    public void SheathWeapon()
+    {
+        _currWeaponInSheath = Instantiate(_weapon, _sheathHolder.transform);
+        Destroy(_currWeaponInHand);
+    }
+}
