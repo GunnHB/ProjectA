314722f9e36b680cb3b50c72a912a7ce0a3ec8f5
GunnHB <gunn0467@gmail.com>
2024-03-04 18:28:17 +0900

    [03/04] 다이얼로그 - 슬라이드 창 ui 제작

--- Assets/02.Scripts/UI/Base/UIButton.cs
+++ Assets/02.Scripts/UI/Base/UIButton.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Image))]
 public class UIButton : Button, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private UnityAction EnterAction;
