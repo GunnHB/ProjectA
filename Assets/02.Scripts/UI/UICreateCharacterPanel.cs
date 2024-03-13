4d2bfb4dd8afae52f5d683c45ab09c8512c229c1
GunnHB <gunn0467@gmail.com>
2024-03-13 12:11:37 +0900

    [03/13] UICreateCharacterPanel 중간 커밋

--- Assets/02.Scripts/UI/UICreateCharacterPanel.cs
+++ Assets/02.Scripts/UI/UICreateCharacterPanel.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using TMPro;
+
 using UnityEngine;
 
-public class UICreateCharacterPanel : MonoBehaviour
+public class UICreateCharacterPanel : UIPanelBase
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string GROUP_SELECT_GENDER = "Select Gender";
+    private const string GROUP_COMMON = "Common";
+
+    [BoxGroup(GROUP_SELECT_GENDER), SerializeField]
+    private UIButtonRaw _maleRawButton;
+    [BoxGroup(GROUP_SELECT_GENDER), SerializeField]
+    private UIButtonRaw _femaleRawButton;
+
+    [BoxGroup(GROUP_COMMON), SerializeField]
+    private TextMeshProUGUI _subtitleText;
+    [BoxGroup(GROUP_COMMON), SerializeField]
+    private UIButton _backButton;
+    [BoxGroup(GROUP_COMMON), SerializeField]
+    private UIButton _nextButton;
+
+    public override void Init()
     {
-        
+        base.Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitRenderTexture()
     {
-        
+
     }
 }
